944c0fe8558709b3538dfca541fcdfba29bfbf38
keroken <kenichi.shinagawa@gmail.com>
2024-05-02 23:52:06 +0900

    style Header with navigation

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,8 +1,18 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="py-5 px-10 border-b flex justify-between items-center">
+      <div>
+        <h1 className="text-2xl font-extrabold">
+          <Link href="/">Next Blog</Link>
+        </h1>
+      </div>
+      <nav className="text-sm font-medium">
+        <Link href="/articles/new" className="bg-blue-100 px-3 py-3 rounded-md">記事を書く</Link>
+      </nav>
+    </header>
   )
 }
 
