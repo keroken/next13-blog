2b6e0a2badb8258cef858540c5abc1e9ca385ec3
keroken <kenichi.shinagawa@gmail.com>
2024-05-03 00:33:06 +0900

    add image to ArticleList

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -1,7 +1,15 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 export const ArticleList = () => {
   return (
-    <div>ArticleList</div>
+    <div>
+      <article>
+        <Link href="#">
+          <Image src="https://source.unsplash.com/collection/1346951/1000x500?sig=1" alt="" width={1280} height={300} />
+        </Link>
+      </article>
+    </div>
   )
 }
